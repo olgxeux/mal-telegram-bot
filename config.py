75c4164936ed9de5692fe65c6d35c308d08b8c87
olgxeux <olgxeux@proton.me>
2023-06-02 17:25:48 +0300

    minor fix

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from dotenv import dotenv_values
 
 env_vars = dotenv_values('.env')
 
-CONNECTION_STRING = env_vars["CONNECTION_STRING"]
-DATABASE_NAME = env_vars["DATABASE_NAME"]
-BOT_TOKEN = env_vars["BOT_TOKEN"]
+CONNECTION_STRING = str(env_vars["CONNECTION_STRING"])
+DATABASE_NAME = str(env_vars["DATABASE_NAME"])
+BOT_TOKEN = str(env_vars["BOT_TOKEN"])
--- database/mongodb_manager.py
+++ database/mongodb_manager.py
@@ -9,7 +9,7 @@ db = client[DATABASE_NAME]
 user_collection = db["User"]
 
 
-def type_to_list_name(media_type: str) -> Optional[str]:
+def _type_to_list_name(media_type: str) -> Optional[str]:
     if media_type == "anime":
         return "AnimeList"
     elif media_type == "manga":
@@ -30,28 +30,28 @@ async def create_new_user(user_id: int) -> dict:
 
 
 async def add_media_for_user(media_type: str, user_id: int, new_media: dict) -> dict:
-    list_name = type_to_list_name(media_type)
+    list_name = _type_to_list_name(media_type)
     user = await user_collection.find_one_and_update(
         {"id": user_id}, {"$push": {list_name: new_media}}, return_document=True)
     return user
 
 
 async def delete_media_from_user(media_type: str, user_id: int, media_id: int) -> dict:
-    list_name = type_to_list_name(media_type)
+    list_name = _type_to_list_name(media_type)
     user = await user_collection.find_one_and_update(
         {"id": user_id}, {"$pull": {list_name: {"Id": media_id}}}, return_document=True)
     return user
 
 
 async def change_users_media_status(media_type: str, user_id: int, media_id: int, new_status: str) -> dict:
-    list_name = type_to_list_name(media_type)
+    list_name = _type_to_list_name(media_type)
     user = await user_collection.find_one_and_update(
         {"id": user_id, f"{list_name}.Id": media_id}, {"$set": {f"{list_name}.$.Status": new_status}}, return_document=True)
     return user
 
 
 async def change_users_media_rating(media_type: str, user_id: int, media_id: int, new_rating: str) -> dict:
-    list_name = type_to_list_name(media_type)
+    list_name = _type_to_list_name(media_type)
     user = await user_collection.find_one_and_update(
         {"id": user_id, f"{list_name}.Id": media_id}, {"$set": {f"{list_name}.$.Rating": new_rating}}, return_document=True)
     return user
