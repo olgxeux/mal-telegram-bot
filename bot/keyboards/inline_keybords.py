e1a37da70ea9437501fb2ce0a3ee5d1fd27f6fc4
olgxeux <olgxeux@proton.me>
2023-06-05 20:02:40 +0300

    change callback text

--- bot/keyboards/inline_keybords.py
+++ bot/keyboards/inline_keybords.py
@@ -25,15 +25,15 @@ async def get_users_medias_kb(type: str, user: dict, page: int, per_page=9) -> I
                                     callback_data=f"view_user_media_{media['Id']}"))
     if page == 0:
         kb.row(InlineKeyboardButton(text=">",
-                                    callback_data=f"view_list_{type}_p_{page + 1}"))  # type: ignore
+                                    callback_data=f"view_users_list_{type}_p_{page + 1}"))  # type: ignore
     elif page == last_page:
         kb.row(InlineKeyboardButton(text="<",
-                                    callback_data=f"view_list_{type}_p_{page - 1}"))  # type: ignore
+                                    callback_data=f"view_users_list_{type}_p_{page - 1}"))  # type: ignore
     else:
         kb.row(InlineKeyboardButton(text="<",
-                                    callback_data=f"view_list_{type}_p_{page - 1}"),  # type: ignore
+                                    callback_data=f"view_users_list_{type}_p_{page - 1}"),  # type: ignore
                InlineKeyboardButton(text=">",
-                                    callback_data=f"view_list_{type}_p_{page + 1}"))  # type: ignore
+                                    callback_data=f"view_users_list_{type}_p_{page + 1}"))  # type: ignore
     kb.row(InlineKeyboardButton(
         text="Back", callback_data=f"view_main_menu"))  # type: ignore
     return kb
